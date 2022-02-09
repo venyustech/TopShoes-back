adb4e109b045ccdb282bbe53072c7c3683648ec6
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-02-08 21:30:19 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express, { json } from "express";
+import cors from "cors";
+
+// import router from "./routes/index.js";
+
+const server = express();
+
+server.use(json());
+server.use(cors());
+// server.use(router);
+
+server.listen(5000, () => console.log("Server in http://localhost:5000/"))
