cc354f2f5beaa2b18e8687da9886704a4e34c549
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-02-13 17:32:09 -0300

    feat: adding get  /api/products/:index

--- src/contollers/getProductsController.js
+++ src/contollers/getProductsController.js
@@ -1,6 +1,6 @@
 
 import products from "../Data/Products.js";
-
+//retorna a lista de produtos
 export async function getProducts(req, res) {
     res.send(products);
 }   
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
+import getProductRouter from "./getProductRouter.js";
 import getProductsRouter from "./getProductsRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(getProductsRouter);
+router.use(getProductRouter)
 
 export default router;
